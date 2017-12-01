348c9362067c32bda26821b92938608fde0a4860
kurorooooo <kurorooooo0927@gmail.com>
2017-12-01 22:32:46 +0900

    20171201_change

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128025943) do
+ActiveRecord::Schema.define(version: 20171201130315) do
 
   create_table "restaurants", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
   create_table "users", force: :cascade do |t|
